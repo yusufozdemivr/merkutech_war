21ae60bbe772b0c4f3449ed41d0e33863734f74a
yusufozdemivr <yusufozdemir.ist@gmail.com>
2024-10-03 10:55:08 +0300

    added marks and info panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapComponent from './components/map_component';
+import InfoPanel from './components/info_panel';
+import './App.css'; // For basic layout styling
 
 function App() {
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
+  const locations = [
+    { id: 1, name: 'Location 1', coords: [31.5, 34.45] },
+    { id: 2, name: 'Location 2', coords: [31.6, 34.55] },
+    { id: 3, name: 'Location 3', coords: [31.7, 34.65] }
+  ];
+
+  const handleLocationClick = (location) => {
+    setSelectedLocation(location.name);
+  };
+
   return (
     <div className="App">
-      <MapComponent />
+      <div className="map-container">
+        <MapComponent locations={locations} onLocationClick={handleLocationClick} />
+      </div>
+      <InfoPanel selectedLocation={selectedLocation} />
     </div>
   );
 }
--- src/components/map_component.js
+++ src/components/map_component.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const MapComponent = () => {
+const MapComponent = ({ locations, onLocationClick }) => {
   return (
-    <MapContainer center={[31.5, 35.0]} zoom={8} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer
+      center={[31.5, 34.5]}
+      zoom={8}
+      style={{ height: '100vh', width: '100%' }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      
+      {locations.map((location) => (
+        <Marker
+          key={location.id}
+          position={location.coords}
+        >
+          <Popup>
+            <div>
+              {location.name}
+            </div>
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
