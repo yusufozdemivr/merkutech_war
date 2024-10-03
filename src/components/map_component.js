a84753228f6d48b49e63db5b39f0f2bc9f057bbc
yusufozdemivr <yusufozdemir.ist@gmail.com>
2024-10-03 10:09:42 +0300

    zoomed world map

--- src/components/map_component.js
+++ src/components/map_component.js
@@ -4,7 +4,7 @@ import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
   return (
-    <MapContainer center={[20, 0]} zoom={2} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[31.5, 35.0]} zoom={8} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
