519f005f2a4002782fa6678d135f01d72b8653a2
yusufozdemivr <yusufozdemir.ist@gmail.com>
2024-10-03 10:01:00 +0300

    created a world map

---
+++ src/components/map_component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MapComponent = () => {
+  return (
+    <MapContainer center={[20, 0]} zoom={2} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
